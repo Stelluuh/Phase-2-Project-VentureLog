eec4bd9806ad6e8a74001fdc0568faf7b05351e9
stella <sftamesis13@gmail.com>
2022-10-26 14:30:33 -0700

    re-install the correct version of react-dom router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
 import Header from './Components/Header'
+import NavBar from './Components/NavBar'
+import Home from './Components/Home'
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+
+    // </Router>
   );
 }
 
