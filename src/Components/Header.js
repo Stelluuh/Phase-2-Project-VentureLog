330b563da325e04783d876a844732cd4d625244f
stella <sftamesis13@gmail.com>
2022-10-26 13:23:54 -0700

    initial commit and add header component

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <div>
+            <h1>VentLog</h1>
+            <h3>A Wonderers Footprint</h3>
+        </div>
+    )
+}
+
+export default Header;
