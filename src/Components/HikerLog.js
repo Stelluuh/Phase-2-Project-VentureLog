cc34665a6d661ecf74b73a4d0ca4cf0536d3c9bf
stella <sftamesis13@gmail.com>
2022-10-26 13:30:57 -0700

    add HikerLog, HikerLogs, Home, and Navbar Component

---
+++ src/Components/HikerLog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HikerLog() {
+    return(
+        <div>
+            <h3>HikerLog Component</h3>
+        </div>
+    )
+}
+
+export default HikerLog;
---
+++ src/Components/HikerLogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HikerLogs() {
+    return(
+        <div>
+            <h3>HikerLogs Component</h3>
+        </div>
+    )
+}
+
+export default HikerLogs;
