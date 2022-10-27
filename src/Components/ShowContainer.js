74e0df1539a28c043037962ef6fc3f1a46a7ba6f
stella <sftamesis13@gmail.com>
2022-10-26 17:10:46 -0700

    add ShowContainer Component

---
+++ src/Components/ShowContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShowContainer() {
+    return(
+        <div>
+            <h1>ShowContainer Component</h1>
+        </div>
+    )
+}
+
+export default ShowContainer;
