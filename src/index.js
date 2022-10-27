a82919c8dba552e5852871a08c67ecb7216b262b
stella <sftamesis13@gmail.com>
2022-10-27 13:08:32 -0700

    edit hierarchy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App.js'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './Components/App.js'
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./Components/App.js";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
