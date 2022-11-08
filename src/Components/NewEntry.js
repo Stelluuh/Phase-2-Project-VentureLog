b393c3956ad4d23fe50475d667d677cb7a280687
stella <sftamesis13@gmail.com>
2022-11-08 11:40:35 -0800

    re-structure form onChange event to minimize clutter

--- src/Components/NewEntry.js
+++ src/Components/NewEntry.js
@@ -11,7 +11,7 @@ function NewEntry() {
 
     function handleSubmit(e) {
         e.preventDefault();
-        // console.log("location:", location)
+        // console.log(e.target.location.value)
         const newItem = {
             "Location": location,
             "Trail": trail,
@@ -41,33 +41,6 @@ function NewEntry() {
             .then(item => console.log({item}))
     }
 
-    
-    function handleNewLocation(e){
-        setLocation(e.target.value)
-    }
-
-    function handleNewTrail(e){
-        setTrail(e.target.value)
-    }
-
-    function handleNewDistance(e) {
-        setDistance(e.target.value)
-    }
-
-    function handleNewCompanions(e){
-        setCompanions(e.target.value)
-    }
-
-    function handleNewImageUrl(e) {
-        setImageUrl(e.target.value)
-    }
-
-    function handleNewMemory(e) {
-        setMemory(e.target.value)
-    }
-
-
-
     return(
       <div className="new_entry_form">
         <div className="title">New Entry </div>
@@ -78,24 +51,24 @@ function NewEntry() {
             <label>Location:</label>
             <input
                 type="text"
-                onChange={handleNewLocation}
                 value={location}
+                onChange={e => setLocation(e.target.value)}
             />
             
             <br/>
             <label>Trail Name:</label>
             <input
                 type="text"
-                onChange={handleNewTrail}
                 value={trail}
+                onChange={e => setTrail(e.target.value)}
             />
             
             <br/>
             <label>Distance:</label>
             <input
                 type="text"
-                onChange={handleNewDistance}
                 value={distance}
+                onChange={e => setDistance(e.target.value)}
 
             />
             
@@ -103,16 +76,16 @@ function NewEntry() {
             <label>Companions:</label>
             <input
                 type="text"
-                onChange={handleNewCompanions}
                 value={companions}
+                onChange={e => setCompanions(e.target.value)}
             />
             
             <br/>
             <label>Image:</label>
             <input
                 type="text"
-                onChange={handleNewImageUrl}
                 value={imageUrl}
+                onChange={e => setImageUrl(e.target.value)}
             />
             
             <br/>
@@ -120,8 +93,8 @@ function NewEntry() {
             <textarea
                 className="memory"
                 type="text"
-                onChange={handleNewMemory}
                 value={memory}
+                onChange={e => setMemory(e.target.value)}
             />
             <br/>
             <button 
@@ -137,3 +110,4 @@ function NewEntry() {
 }
 
 export default NewEntry;
+
