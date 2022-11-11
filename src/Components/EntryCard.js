15514642d0c0762e7c020c17068fdfbc6cda548e
stella <sftamesis13@gmail.com>
2022-11-10 16:34:53 -0800

    create Card.css file and transfer styles from index.css

--- src/Components/EntryCard.js
+++ src/Components/EntryCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../stylesheets/Card.css'
 
 function EntryCard({ entry }) {
     // console.log({entry})
--- src/Components/EntryDetails.js
+++ src/Components/EntryDetails.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import '../stylesheets/Card.css'
 
 function EntryDetails() {
     const [entry, setEntry] = useState('')
