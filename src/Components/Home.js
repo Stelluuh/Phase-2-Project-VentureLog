b5ae18b01e4515b1e0fd421e2723b1e8c3d9dcfc
stella <sftamesis13@gmail.com>
2022-11-14 13:29:45 -0800

    edit paragraph of VentureLog Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,8 @@ function Home() {
             <Header />
             <div className='home-container'>
                 <h3>A Wonderers Footprint</h3>
-                <p>This Application allows adventurers to document their hiking journey through multiple trails.</p>
+                <p>A vast, still wilderness of forest, mountains, and prairie, sprinkled with lakes and ponds, and threaded by streams and deer trails, awaits you.</p>
+                <p>Join us as we share in a collective celebration of nature and its wonders documented in our online Venture Log. We look forward to hearing about your journeys and discoveries!</p>
             </div>
         </div>  
     )
