6f1319eb17b46e82cc74566bf5b883412241120c
stella <sftamesis13@gmail.com>
2022-11-20 12:44:48 -0800

    remove unnecessary comment

--- src/index.js
+++ src/index.js
@@ -11,17 +11,3 @@ ReactDOM.render(
   </BrowserRouter>, 
   document.getElementById('root'))
 
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./Components/App.js";
-// import reportWebVitals from "./reportWebVitals";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// reportWebVitals();
